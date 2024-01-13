95acccaa5d718f1eb9708610a10fdd2f09f15b4e
karmalover-ca <liamphone0@gmail.com>
2024-01-12 20:15:14 -0500

    adds double solenoid for shifting gearbox

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Drivetrain extends SubsystemBase {
@@ -9,9 +10,30 @@ public class Drivetrain extends SubsystemBase {
     CANSparkMax leftLeadController;
     CANSparkMax rightLeadController;
 
-    public Drivetrain(CANSparkMax leftLeadController, CANSparkMax rightLeadController) {
+    DoubleSolenoid shiftSolenoid;
+
+    Gear currentGear;
+
+    public Drivetrain(CANSparkMax leftLeadController, CANSparkMax rightLeadController, DoubleSolenoid shiftSolenoid) {
         this.leftLeadController = leftLeadController;
         this.rightLeadController = rightLeadController;
+        this.shiftSolenoid = shiftSolenoid;
+    }
+
+    public enum Gear {
+        HIGH(DoubleSolenoid.Value.kForward),
+        LOW(DoubleSolenoid.Value.kReverse),
+        OFF(DoubleSolenoid.Value.kOff);
+
+        private DoubleSolenoid.Value value;
+
+        private Gear(DoubleSolenoid.Value value) {
+            this.value = value;
+        }
+
+        public DoubleSolenoid.Value getValue() {
+            return value;
+        }
     }
 
     public void setOutput(double left, double right) {
@@ -22,6 +44,15 @@ public class Drivetrain extends SubsystemBase {
     public void stop() {
         leftLeadController.set(0);
         rightLeadController.set(0);
+    }
+
+    public void setGear(Gear gear) {
+        currentGear = gear;
+        shiftSolenoid.set(gear.value);
+    }
+
+    public Gear getGear() {
+        return currentGear;
     }
     
 }
