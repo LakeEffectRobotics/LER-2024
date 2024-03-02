00fa9a85425d25d294acd7170de3f5ae45866e81
Liam Pilson <liamphone0@gmail.com>
2024-03-01 20:05:26 -0500

    adds motor controllers for intake to robotmap

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -12,11 +12,25 @@ public class RobotMap {
 
     /** Inner class to hold CAN ID constants */
     private class CAN {
-
+        /** Drivetrain */
         private static final int LEFT_CONTROLLER_1 = 3;
         private static final int LEFT_CONTROLLER_2 = 4;
         private static final int RIGHT_CONTROLLER_1 = 1;
         private static final int RIGHT_CONTROLLER_2 = 2;
+
+        /** Intake */
+        
+        // Climber
+        private static final int CLIMB_CONTROLLER_1 = 5;
+        private static final int CLIMB_CONTROLLER_2 = 6;
+        private static final int CLIMB_CONTROLLER_3 = 7;
+
+        private static final int ARM_CONTROLLER_1 = 8;
+        private static final int ARM_CONTROLLER_2 = 9;
+
+        private static final int CLAW_CONTROLLER = 10;
+        private static final int WRIST_CONTROLLER = 11;
+
     }
 
     private class PCM {
@@ -24,21 +38,44 @@ public class RobotMap {
         private static final int DRIVE_SHIFT_DOWN = 15;
     }
 
+    /** Drivetrain */
     public static final CANSparkMax leftController1 = new CANSparkMax(CAN.LEFT_CONTROLLER_1, MotorType.kBrushless);
     public static final CANSparkMax rightController1 = new CANSparkMax(CAN.RIGHT_CONTROLLER_1, MotorType.kBrushless);
     public static final CANSparkMax rightController2 = new CANSparkMax(CAN.RIGHT_CONTROLLER_2, MotorType.kBrushless);
     public static final CANSparkMax leftController2 = new CANSparkMax(CAN.LEFT_CONTROLLER_2, MotorType.kBrushless);
-
     public static final DoubleSolenoid driveShitSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, PCM.DRIVE_SHIFT_DOWN, PCM.DRIVE_SHIFT_UP);
+
+    /** Intake */
+        
+    // Climber
+    public static final CANSparkMax climbController1 = new CANSparkMax(CAN.CLIMB_CONTROLLER_1, MotorType.kBrushless);
+    public static final CANSparkMax climbController2 = new CANSparkMax(CAN.CLIMB_CONTROLLER_2, MotorType.kBrushless);
+    public static final CANSparkMax climbController3 = new CANSparkMax(CAN.CLIMB_CONTROLLER_3, MotorType.kBrushless);
+
+    private static final CANSparkMax armController1 = new CANSparkMax(CAN.ARM_CONTROLLER_1, MotorType.kBrushless);
+    private static final CANSparkMax armController2 = new CANSparkMax(CAN.ARM_CONTROLLER_2, MotorType.kBrushless);
+
+    private static final CANSparkMax clawController = new CANSparkMax(CAN.CLAW_CONTROLLER, MotorType.kBrushless);
+    private static final CANSparkMax wristController = new CANSparkMax(CAN.WRIST_CONTROLLER, MotorType.kBrushless);
+
 
     // Static initializer will be run on first reference to RobotMap (stealing code from greg)
     static {
         
+        /** Drivetrain */
         leftController2.follow(leftController1);
         rightController2.follow(rightController1);
 
         leftController1.setInverted(false);
         rightController1.setInverted(true);   
+
+        /** Intake */
+        
+        // Climber 
+        climbController2.follow(climbController1);
+        climbController3.follow(climbController1);
+
+        armController2.follow(armController1);
     }
     
 }
